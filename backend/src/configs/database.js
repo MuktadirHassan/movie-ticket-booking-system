910b10bb9be610964317cee220beeefae67d5c36
MuktadirHassan <tamimmuktadir@gmail.com>
2023-06-09 22:02:42 +0600

    added db connection env variables

--- backend/src/configs/database.js
+++ backend/src/configs/database.js
@@ -1,5 +1,11 @@
-// create the connection to database
+import postgres from "postgres";
 
-const db = {};
+const sql = postgres({
+  host: process.env.db_host,
+  port: process.env.db_port,
+  database: process.env.database,
+  username: process.env.db_username,
+  password: process.env.db_password,
+});
 
-export default db;
+export default sql;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,21 @@
+import "dotenv/config.js";
 import express from "express";
 import morgan from "morgan";
-import db from "./configs/database.js";
+import sql from "./configs/database.js";
 
 import logger from "./utils/logger.js";
 import { PORT } from "./configs/appConfig.js";
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan(""));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  const conn = await sql`SELECT 1+1 AS result`;
+  logger.info(`Database connected: ${conn[0].result === 2}`);
   logger.info(`Server is running on port ${PORT}`);
 });
