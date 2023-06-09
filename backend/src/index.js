72d2bdf9fe0cdcb39601ad98f8e261dcf1ae6871
MuktadirHassan <tamimmuktadir@gmail.com>
2023-06-09 17:15:26 +0600

    updated logging

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 import db from "./configs/database.js";
+import loggerLevels from "./constants/loggerLevels.js";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  logger.info(`Server started on port ${process.env.PORT || 3000}`);
+  console.log(
+    `[${
+      loggerLevels.info
+    }] [${new Date().toLocaleString()}] Server is running at port ${
+      process.env.PORT || 3000
+    }`
+  );
 });
