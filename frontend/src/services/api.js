2a1d6976888f49f788612d1946e1a29e53518a57
MuktadirHassan <tamimmuktadir@gmail.com>
2023-06-10 20:22:10 +0600

    added new api routes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -38,3 +38,58 @@ export const getUser = async () => {
   const response = await api.get("/me");
   return response.data;
 };
+
+export const getMovies = async () => {
+  const response = await api.get("/movies");
+  return response.data;
+};
+
+export const getMovie = async (id) => {
+  const response = await api.get(`/movies/${id}`);
+  return response.data;
+};
+
+export const createMovie = async (movie) => {
+  const response = await api.post("/movies", movie);
+  return response.data;
+};
+
+export const updateMovie = async (id, movie) => {
+  const response = await api.put(`/movies/${id}`, movie);
+  return response.data;
+};
+
+export const deleteMovie = async (id) => {
+  const response = await api.delete(`/movies/${id}`);
+  return response.data;
+};
+
+export const createHall = async (hall) => {
+  const response = await api.post("/halls/create", hall);
+  return response.data;
+};
+
+export const getHalls = async () => {
+  const response = await api.get("/halls");
+  return response.data;
+};
+
+export const getHall = async (id) => {
+  const response = await api.get(`/halls/${id}`);
+  return response.data;
+};
+
+export const createShow = async (show) => {
+  const response = await api.post("/shows/create", show);
+  return response.data;
+};
+
+export const getShows = async () => {
+  const response = await api.get("/shows");
+  return response.data;
+};
+
+export const getBookings = async () => {
+  const response = await api.get("/bookings");
+  return response.data;
+};
