c237e380447f588c958537c834a084b176026977
MuktadirHassan <tamimmuktadir@gmail.com>
2023-06-19 14:58:57 +0600

    updated schema

--- frontend/src/routes/Movie.jsx
+++ frontend/src/routes/Movie.jsx
@@ -1,8 +1,9 @@
-import { Container, Paper, Typography } from "@mui/material";
+import { Container, Paper, Tab, Tabs, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import { getMovie } from "../services/api";
 import { useQuery } from "@tanstack/react-query";
 import { FormatDate } from "../utils/formatDate";
+import { useState } from "react";
 
 export default function Movie() {
   const { movieId } = useParams();
@@ -35,26 +36,42 @@ const MovieDetails = ({ movie }) => {
     <Paper
       sx={{
         padding: 2,
+        display: "flex",
+        flexDirection: "column",
+        gap: 1,
       }}
+      elevation={3}
     >
       <Typography variant="h4">{movie.movie_name}</Typography>
+      <Typography variant="body2">{FormatDate(movie.release_date)}</Typography>
+      <Typography variant="body2">{movie.duration} minutes</Typography>
       <Typography variant="body">{movie.description}</Typography>
-      <Typography variant="body">{movie.duration} minutes</Typography>
-      <Typography variant="body">{FormatDate(movie.release_date)}</Typography>
     </Paper>
   );
 };
 
 const MovieShowtimes = ({ shows }) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   if (!shows) {
     return <div>No showtimes available</div>;
   }
 
   return (
     <div>
-      {shows.map((show) => {
-        return <div key={show.show_id}>{FormatDate(show.show_date)}</div>;
-      })}
+      <Tabs value={value} onChange={handleChange} centered>
+        {shows.map((show) => {
+          return (
+            <Tab key={show.show_id} label={FormatDate(show.show_date)}>
+              {FormatDate(show.show_date)}
+            </Tab>
+          );
+        })}
+      </Tabs>
     </div>
   );
 };
---
+++ frontend/src/routes/admin/MovieManagement.jsx
@@ -0,0 +1,8 @@
+export default function MovieManagement() {
+  // create, update, delete movies
+  return (
+    <div>
+      <h1>Movie Management</h1>
+    </div>
+  );
+}
